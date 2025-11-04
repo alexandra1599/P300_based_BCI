f53b0de40862f2e2975942b0258174b9fd562069
alexandra1599 <amikhael@utexas.edu>
2025-11-03 20:16:42 -0600

    Create udp_send.py

---
+++ Visual Interface /udp_send.py
@@ -0,0 +1,23 @@
+import socket
+
+# Server address and port (update as needed)
+server_address = '127.0.0.1'  # Change to the IP of the FES script's machine
+server_port = 12345          # Match the port used in the FES script
+server_address_port = (server_address, server_port)
+
+buffer_size = 1024
+
+# Create a UDP socket on the client side
+udp_client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+print(f"Sending UDP commands to {server_address}:{server_port}")
+while True:
+    val = input("Enter message: ")
+
+    # Exit the program
+    if val.lower() == "e":
+        print("Ending program, bye...")
+        break
+
+    print("Message to send: ", val)
+    udp_client_socket.sendto(val.encode(), server_address_port)
